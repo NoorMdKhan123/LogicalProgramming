59a82e6ab207392aaa62163661bb5cf20130c7e1
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 03:30:13 +0530

    Checking prime number

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check prime");
+            int num = int.Parse(Console.ReadLine());
+            int temp = 0;
+            for( int i=2 ; i<num-1 ; i++)
+            {
+                if(num%i == 0)
+                {
+                    temp += 1;
+                }
+            }
+            if(temp > 0)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+            else
+            {
+                Console.WriteLine("Number is not prime number");
+
+            }
+
+        }
+    }
+}
