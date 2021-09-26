eb596cf24bd6442f36aafeed9316e9ced2e0c30a
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 03:36:00 +0530

    Calculating elapsed time between two given time period

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWatchProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopw = new Stopwatch();
+            stopw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(20);
+            }
+            stopw.Stop();
+            Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
+        }
+    
+    }
+}
