750a52027462bfd00651f0dfe1ec776256eaf9d7
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 03:24:39 +0530

    Calculated Fibinocci Series

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1=0, num2=1, num3;
+            Console.WriteLine("Enter the number of elements");
+          int number = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2  +" ");
+            for(int i = 2; i<number; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+
+            }
+
+        }
+    }
+}
