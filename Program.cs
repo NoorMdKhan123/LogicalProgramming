eb731ef306919f5daf5eb9248ece23ca468467e6
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 03:33:57 +0530

    Program to reverse a given number

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  rem, rev = 0;
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            
+            while (num!= 0)
+            {
+              
+                rem = num % 10;
+                rev = (rev * 10)+ rem;
+                num = num / 10;
+            }
+            Console.WriteLine("reverse number is" + rev);
+        }
+    }
+}
