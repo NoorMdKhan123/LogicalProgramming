08c603234947c2a57c8b7883b178d832135baf57
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-27 03:27:50 +0530

    Perfect number evaluation

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i=1; i<num; i++)
+            {
+                if(num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("Number is perfect number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not prime number");
+            }
+        }
+    }
+}
